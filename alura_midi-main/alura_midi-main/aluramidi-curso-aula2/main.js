ffe5557b177a1fc12f66aea9aa520df7c4b9fbfc
maria jaqueline <112712411+mariafavato@users.noreply.github.com>
2023-10-19 11:04:36 -0300

    alura_midi

--- alura_midi-main/alura_midi-main/aluramidi-curso-aula2/main.js
+++ alura_midi-main/alura_midi-main/aluramidi-curso-aula2/main.js
@@ -5,13 +5,12 @@ function tocaSomPom () {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
 
-listaDeTeclas[1].onclick = tocaSomClap;
-listaDeTeclas[2].onclick = tocaSomTim;
-listaDeTeclas[3].onclick = tocaSomPuff;
-listaDeTeclas[4].onclick = tocaSomSplash;
-listaDeTeclas[5].onclick = tocaSomToim;
-listaDeTeclas[6].onclick = tocaSomPsh;
-listaDeTeclas[7].onclick = tocaSomTic;
-listaDeTeclas[8].onclick = tocaSomTom;
+while (contador = 0) {
+    listaDeTeclas[0].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
