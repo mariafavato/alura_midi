b4a917f0ce2900cb874dc52b3fb23af216840214
maria jaqueline <112712411+mariafavato@users.noreply.github.com>
2023-10-23 09:08:59 -0300

    Add files via upload

---
+++ alura_midi-main/alura_midi-main/alura_midi-main/aluramidi-curso-aula2/main.js
@@ -0,0 +1,18 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador = listaDeTeclas.length) {
+
+    listaDeTeclas[contador].onclick = tocaSom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
--- alura_midi-main/alura_midi-main/aluramidi-curso-aula2/main.js
+++ alura_midi-main/alura_midi-main/aluramidi-curso-aula2/main.js
@@ -2,15 +2,4 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
-let contador = 0;
-
-while (contador = 0) {
-    listaDeTeclas[0].onclick = tocaSomPom;
-
-    contador = contador + 1;
-
-    console.log(contador);
-}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
